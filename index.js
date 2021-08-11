422df855ccb674298dcc0ef9bce5e1dc9eb2c4e1
William Mosto <will.mosto@gmail.com>
2021-08-11 19:05:44 -0400

    commit tree 2e64434ffaaf19dc5ff12d927bc3675b8c1b8e3f

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+function findMatching (driverArray,driverName) {
+return driverArray.filter(element => 
+element.toUpperCase() === driverName.toUpperCase())
+}
+
+function fuzzyMatch (driverArray,driverName) {
+return driverArray.filter(element =>
+element.slice(0,1) === driverName.slice(0,1))
+}
+
+function matchName (driverObj,driverName) {
+return driverObj.filter(element =>
+    element.name === driverName)
+}
+
